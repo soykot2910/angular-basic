c1ff63411670bf9decf74bcc2a634329934448fc
soykot2910 <md.soykot2910@gmail.com>
2022-09-11 20:56:56 +0600

    sidebar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from '../app/components/authors/authors.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
